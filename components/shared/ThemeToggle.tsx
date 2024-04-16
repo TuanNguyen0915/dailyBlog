02fc21538e49aad5872342998eb73c85e9d3c026
Tuan Nguyen <tuan.nat915@gmail.com>
2024-04-16 12:01:47 -0400

    edited themeToggle

--- components/shared/ThemeToggle.tsx
+++ components/shared/ThemeToggle.tsx
@@ -1,26 +1,22 @@
 "use client"
-import { Moon, Sun } from "lucide-react"
+
 import { useTheme } from "next-themes"
-import { Switch } from "../ui/switch"
+import { RiMoonClearFill, RiSunFill } from "react-icons/ri"
+import { Button } from "../ui/button"
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <div
-      className={`border-border flexBetween relative gap-4 rounded-2xl border px-4 py-2`}
+    <Button
+      variant="ghost"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <div
-        className={`${theme === "dark" ? "right-0" : "left-0"} absolute top-0 h-full w-1/2 rounded-2xl bg-primary transition-all duration-500`}
-      ></div>
-      <Moon size={15}
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        className="text-primary cursor-pointer"
-      />
-      <Sun size={15}
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        className="text-foreground cursor-pointer"
-      />
-    </div>
+      {theme === "dark" ? (
+        <RiSunFill size={24} />
+      ) : (
+        <RiMoonClearFill size={24} />
+      )}
+    </Button>
   )
 }
